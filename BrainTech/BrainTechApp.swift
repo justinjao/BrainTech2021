7cef9ba3a093b163213327d9b450691803009cee
justinjao <jao.c.justin@gmail.com>
2021-08-13 10:18:00 -0700

    Initial Commit

---
+++ BrainTech/BrainTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTechApp.swift
+//  BrainTech
+//
+//  Created by Justin Jao on 2021-1308-.
+//
+
+import SwiftUI
+
+@main
+struct BrainTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainTech/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrainTech
+//
+//  Created by Justin Jao on 2021-1308-.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
